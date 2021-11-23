52cbfd27af36c013fbda2d2fb11bbde706f28fb6
Martín <martinngaleano93@gmail.com>
2021-11-22 23:50:07 -0300

    Proyecto Inicial1

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,66 +1,39 @@
-function cervezas(){
-    let marcas = [{
-        nombre: "heineken",
-        precio: 250
-    },{
-        nombre: "quilmes",
-        precio: 180
-    },{
-        nombre: "andes",
-        precio: 220
-    }]
+let heineken = {id:1, precio:250}
+let quilmes = {id:2, precio:180}
+let andes = {id:3, precio:220}
+let cervezas = [heineken, quilmes, andes];
 
-    function calculo(){
-        let opciones = parseInt(prompt("¿Qué marca prefiere? \n 1- Heineken \n 2 - Quilmes \n 3- Andes"));
-        let opcionfinal = opciones - 1;
-        let cantidad = parseInt(prompt("¿Cuántas cervezas quiere comprar?"));
-        let precio = cantidad * marcas[opcionfinal].precio;
-        return alert("Usted debe abonar $" + precio);
-    }
-    calculo();
+function calculoc(){
+    let opciones = parseInt(prompt("¿Qué marca prefiere? \n 1- Heineken \n 2 - Quilmes \n 3- Andes"));
+    let opcionfinal = opciones - 1;
+    let cantidad = parseInt(prompt("¿Cuántas cervezas quiere comprar?"));
+    let precio = cantidad * cervezas[opcionfinal].precio;
+    return alert("Usted debe abonar $" + precio);
 }
+let benjamin = {id:4, precio:380}
+let callia = {id:5, precio:320}
+let emilia = {id:6, precio:450}
+let vinos = [benjamin, callia, emilia]
 
-function vinos(){
-    let marcas = [{
-        nombre: "benjamín",
-        precio: 380
-    },{
-        nombre: "callia",
-        precio: 320
-    },{
-        nombre: "emilia",
-        precio: 450
-    }]
-    function calculo(){
+function calculov(){
         let opciones = parseInt(prompt("¿Qué marca prefiere? \n 1- Benjamín \n 2 - Callia \n 3- Emilia"));
         let opcionfinal = opciones - 1;
         let cantidad = parseInt(prompt("¿Cuántos vinos quiere comprar?"));
-        let precio = cantidad * marcas[opcionfinal].precio;
+        let precio = cantidad * vinos[opcionfinal].precio;
         return alert("Usted debe abonar $" + precio);
-    }
-    calculo();
-
 }
-function aperitivos(){
-    let marcas = [{
-        nombre: "fernet",
-        precio: 750
-    },{
-        nombre: "gancia",
-        precio: 400
-    },{
-        nombre: "aperol",
-        precio: 650
-    }]
-    function calculo(){
+let fernet = {id:7, precio: 750};
+let gancia = {id:8, precio: 400};
+let aperol = {id:9, precio: 650};
+let aperitivos = [fernet, gancia, aperol];
+
+function calculoa(){
         let opciones = parseInt(prompt("¿Qué bebida prefiere? \n 1- Fernet \n 2 - Gancia \n 3- Aperol"));
         let opcionfinal = opciones - 1;
         let cantidad = parseInt(prompt("¿Cuántas botellas desea?"));
-        let precio = cantidad * marcas[opcionfinal].precio;
+        let precio = cantidad * aperitivos[opcionfinal].precio;
         return alert("Usted debe abonar $" + precio);
-    }
-    calculo();
-}
+ }
 
 
 
@@ -71,13 +44,13 @@ if (edad >= 18){
     let bebida = prompt("¿Qué desea tomar hoy? \n 1 - Cerveza \n 2 - Vino \n 3 - Aperitivo");
 switch (bebida){
     case "1":
-        cervezas();
+        calculoc();
         break;
      case "2":
-        vinos();
+        calculov();
         break;
     case "3":
-        aperitivos();
+        calculoa();
         break;
     default:
         break;
